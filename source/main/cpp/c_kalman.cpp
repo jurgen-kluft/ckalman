525a422e8e53faa524c44dd6fd248e24e42ba368
Jurgen Kluft2 <jurgen@virtuosgames.com>
2025-10-27 16:14:07 +0800

    * ckalman
    * more implementation (WIP)

--- source/main/cpp/c_kalman.cpp
+++ source/main/cpp/c_kalman.cpp
@@ -1,198 +1,212 @@
-#include "ccore/c_allocator.h"
 #include "ckalman/c_kalman.h"
 
 namespace ncore
 {
-    namespace nmath
-    {
-        struct Vector
-        {
-            s32    N;
-            s32    Inc;
-            float* data;
-
-            s32 Len() const { return N; }
-
-            void MulVec(Matrix* m, Vector* v)
-            {
-                // TODO implement matrix-vector multiplication
-                // Note: Incoming vector v can be the same as this!
-            }
-
-            void AddVec(Vector* m, Vector* v)
-            {
-                // TODO implement vector addition
-                // Note: Incoming vector m or v can be the same as this!
-            }
-
-            void SubVec(Vector* m, Vector* v)
-            {
-                // TODO implement vector subtraction
-                // Note: Incoming vector m or v can be the same as this!
-            }
-        };
-
-        struct Matrix
-        {
-            s32    Rows;
-            s32    Cols;
-            float* data;
-            s32    stride;
-            s32    capRows;
-            s32    capCols;
-
-            void Set(s32 row, s32 col, float value) { data[row * stride + col] = value; }
-
-            void Product(Matrix* T, Matrix* P, Matrix* transposedT)
-            {
-                // TODO implement matrix-matrix multiplication
-            }
-
-            void Add(Matrix* a, Matrix* b)
-            {
-                // TODO implement matrix addition
-            }
-        };
-
-        Vector* NewVector(s32 n, float* data)
-        {
-            // TODO memory allocation
-            Vector* v = nullptr;
-            v->N      = n;
-            v->Inc    = 1;
-            if (data == nullptr)
-            {
-                // TODO memory allocation
-                // v->data = (float*)malloc(sizeof(float) * n);
-                // all elements initialized to zero
-            }
-            else
-            {
-                v->data = data;
-            }
-            return v;
-        }
-
-        Matrix* NewMatrix(s32 rows, s32 cols, float* data)
-        {
-            // TODO memory allocation
-            Matrix* m = nullptr;
-            m->Rows   = rows;
-            m->Cols   = cols;
-            m->stride = cols;
-            if (data == nullptr)
-            {
-                // TODO memory allocation
-                // m->data = (float*)malloc(sizeof(float) * rows * cols);
-                // all elements initialized to zero
-            }
-            else
-            {
-                m->data = data;
-            }
-            return m;
-        }
-
-        Vector* Copy(Vector* v)
-        {
-            Vector* copy = NewVector(v->N, nullptr);
-            for (s32 i = 0; i < v->N; i++)
-            {
-                copy->data[i] = v->data[i];
-            }
-            return copy;
-        }
-
-        void CopyContent(Vector* dest, Vector* src)
-        {
-            for (s32 i = 0; i < src->N; i++)
-            {
-                dest->data[i] = src->data[i];
-            }
-        }
-
-        void CopyContent(Matrix* dest, Matrix* src)
-        {
-            for (s32 i = 0; i < src->Rows; i++)
-            {
-                for (s32 j = 0; j < src->Cols; j++)
-                {
-                    dest->data[i * dest->stride + j] = src->data[i * src->stride + j];
-                }
-            }
-        }
-
-        Matrix* Copy(Matrix* m)
-        {
-            Matrix* copy = NewMatrix(m->Rows, m->Cols, nullptr);
-            for (s32 i = 0; i < m->Rows; i++)
-            {
-                for (s32 j = 0; j < m->Cols; j++)
-                {
-                    copy->data[i * copy->stride + j] = m->data[i * m->stride + j];
-                }
-            }
-            return copy;
-        }
-
-        Matrix* Transpose(Matrix* m)
-        {
-            Matrix* transposed = NewMatrix(m->Cols, m->Rows, nullptr);
-            for (s32 i = 0; i < m->Rows; i++)
-            {
-                for (s32 j = 0; j < m->Cols; j++)
-                {
-                    transposed->data[j * transposed->stride + i] = m->data[i * m->stride + j];
-                }
-            }
-            return transposed;
-        }
-
-        void Free(Vector* v)
-        {
-            // TODO free memory
-            // free(v->data);
-            // free(v);
-        }
-
-        void Free(Matrix* m)
-        {
-            // TODO free memory
-            // free(m->data);
-            // free(m);
-        }
-
-    }  // namespace nmath
-
     namespace nkalman
     {
-        // NewKalmanFilter returns a new KalmanFilter for the given linear model.
-        KalmanFilter* NewKalmanFilter(nmodels::LinearModel* model)
+        namespace nmath
         {
-            nmodels::State initial;
+            struct vector_t
+            {
+                s32    N;
+                s32    Inc;
+                float* data;
+
+                s32 Len() const { return N; }
+
+                void MulVec(matrix_t* m, vector_t* v)
+                {
+                    // TODO implement matrix-vector multiplication
+                    // Note: Incoming vector v can be the same as this!
+                }
+
+                void AddVec(vector_t* m, vector_t* v)
+                {
+                    // TODO implement vector addition
+                    // Note: Incoming vector m or v can be the same as this!
+                }
+
+                void SubVec(vector_t* m, vector_t* v)
+                {
+                    // TODO implement vector subtraction
+                    // Note: Incoming vector m or v can be the same as this!
+                }
+            };
+
+            struct matrix_t
+            {
+                s32    Rows;
+                s32    Cols;
+                float* data;
+                s32    stride;
+                s32    capRows;
+                s32    capCols;
+
+                void Set(s32 row, s32 col, float value) { data[row * stride + col] = value; }
+
+                void Product(matrix_t* T, matrix_t* P, matrix_t* transposedT)
+                {
+                    // TODO implement matrix-matrix multiplication
+                }
+
+                void Add(matrix_t* a, matrix_t* b)
+                {
+                    // TODO implement matrix addition
+                }
+
+                void Sub(matrix_t* a, matrix_t* b)
+                {
+                    // TODO implement matrix subtraction
+                }
+
+                void Mul(matrix_t* a, matrix_t* b)
+                {
+                    // TODO implement matrix multiplication
+                }
+
+                void Inverse(matrix_t* m)
+                {
+                    // TODO implement matrix inversion
+                }
+            };
+
+            vector_t* NewVector(s32 n, float* data)
+            {
+                // TODO memory allocation
+                vector_t* v = nullptr;
+                v->N      = n;
+                v->Inc    = 1;
+                if (data == nullptr)
+                {
+                    // TODO memory allocation
+                    // v->data = (float*)malloc(sizeof(float) * n);
+                    // all elements initialized to zero
+                }
+                else
+                {
+                    v->data = data;
+                }
+                return v;
+            }
+
+            matrix_t* NewMatrix(s32 rows, s32 cols, float* data)
+            {
+                // TODO memory allocation
+                matrix_t* m = nullptr;
+                m->Rows     = rows;
+                m->Cols     = cols;
+                m->stride   = cols;
+                if (data == nullptr)
+                {
+                    // TODO memory allocation
+                    // m->data = (float*)malloc(sizeof(float) * rows * cols);
+                    // all elements initialized to zero
+                }
+                else
+                {
+                    m->data = data;
+                }
+                return m;
+            }
+
+            vector_t* Copy(vector_t* v)
+            {
+                vector_t* copy = NewVector(v->N, nullptr);
+                for (s32 i = 0; i < v->N; i++)
+                {
+                    copy->data[i] = v->data[i];
+                }
+                return copy;
+            }
+
+            void CopyContent(vector_t* dest, vector_t* src)
+            {
+                for (s32 i = 0; i < src->N; i++)
+                {
+                    dest->data[i] = src->data[i];
+                }
+            }
+
+            void CopyContent(matrix_t* dest, matrix_t* src)
+            {
+                for (s32 i = 0; i < src->Rows; i++)
+                {
+                    for (s32 j = 0; j < src->Cols; j++)
+                    {
+                        dest->data[i * dest->stride + j] = src->data[i * src->stride + j];
+                    }
+                }
+            }
+
+            matrix_t* Copy(matrix_t* m)
+            {
+                matrix_t* copy = NewMatrix(m->Rows, m->Cols, nullptr);
+                for (s32 i = 0; i < m->Rows; i++)
+                {
+                    for (s32 j = 0; j < m->Cols; j++)
+                    {
+                        copy->data[i * copy->stride + j] = m->data[i * m->stride + j];
+                    }
+                }
+                return copy;
+            }
+
+            matrix_t* Transpose(matrix_t* m)
+            {
+                matrix_t* transposed = NewMatrix(m->Cols, m->Rows, nullptr);
+                for (s32 i = 0; i < m->Rows; i++)
+                {
+                    for (s32 j = 0; j < m->Cols; j++)
+                    {
+                        transposed->data[j * transposed->stride + i] = m->data[i * m->stride + j];
+                    }
+                }
+                return transposed;
+            }
+
+            void Free(vector_t* v)
+            {
+                // TODO free memory
+                // free(v->data);
+                // free(v);
+            }
+
+            void Free(matrix_t* m)
+            {
+                // TODO free memory
+                // free(m->data);
+                // free(m);
+            }
+
+        }  // namespace nmath
+
+        // NewKalmanFilter returns a new filter_t for the given linear model.
+        filter_t* NewFilter(nmodels::model_t* model)
+        {
+            nmodels::state_t initial;
             model->InitialState(initial);
 
-            KalmanFilter* km = nullptr;
+            filter_t* km = nullptr;
 
             km->model      = model;
-            km->dims       = initial.State->Len();
+            km->dims       = initial.state_t->Len();
             km->t          = initial.Time;
-            km->state      = nmath::Copy(initial.State);
+            km->state      = nmath::Copy(initial.state_t);
             km->covariance = nmath::Copy(initial.Covariance);
 
             return km;
         }
 
-        nmath::Vector* State(KalmanFilter* kf) { return kf->state; }
-        nmath::Matrix* Covariance(KalmanFilter* kf) { return kf->covariance; }
+        nmath::vector_t*   state_t(filter_t* kf) { return kf->state; }
+        nmath::matrix_t* Covariance(filter_t* kf) { return kf->covariance; }
 
-        void SetCovariance(KalmanFilter* kf, nmath::Matrix* covariance) { nmath::CopyContent(kf->covariance, covariance); }
+        void SetCovariance(filter_t* kf, nmath::matrix_t* covariance) { nmath::CopyContent(kf->covariance, covariance); }
 
-        void SetState(KalmanFilter* kf, nmath::Vector* state) { nmath::CopyContent(kf->state, state); }
+        void SetState(filter_t* kf, nmath::vector_t* state) { nmath::CopyContent(kf->state, state); }
 
-        u64 Time(KalmanFilter* kf) { return kf->t; }
+        u64 Time(filter_t* kf) { return kf->t; }
 
-        bool Predict(KalmanFilter* kf, u64 t)
+        bool Predict(filter_t* kf, u64 t)
         {
             if (t <= kf->t)
                 return false;  // can't predict past
@@ -203,31 +217,24 @@ namespace ncore
             const u64 dt = t - kf->t;
             kf->t        = t;
 
-            nmath::Matrix *T, *Q;
+            nmath::matrix_t *T, *Q;
             kf->model->Transition(dt, T);
             kf->model->CovarianceTransition(dt, Q);
-            nmath::Matrix* P = kf->covariance;
+            nmath::matrix_t* P = kf->covariance;
 
-            nmath::Vector* currState = nmath::Copy(kf->state);
+            nmath::vector_t* currState = nmath::Copy(kf->state);
             kf->state->MulVec(T, currState);
             nmath::Free(currState);
 
-            nmath::Matrix* newCovariance = nmath::NewMatrix(kf->dims, kf->dims, nullptr);
-            nmath::Matrix* transposedT   = nmath::Transpose(T);
+            nmath::matrix_t* newCovariance = nmath::NewMatrix(kf->dims, kf->dims, nullptr);
+            nmath::matrix_t* transposedT   = nmath::Transpose(T);
             newCovariance->Product(T, P, transposedT);
             kf->covariance->Add(newCovariance, Q);
 
             return true;
         }
 
-        // struct Measurement
-        // {
-        //     nmath::Matrix* Covariance;
-        //     nmath::Vector* Value;
-        //     nmath::Matrix* ObservationModel;
-        // };
-
-        bool Update(KalmanFilter* kf, u64 t, nmodels::Measurement* m)
+        bool Update(filter_t* kf, u64 t, nmodels::measurement_t* m)
         {
             if (t <= kf->t)
                 return false;  // can't predict past
@@ -235,44 +242,50 @@ namespace ncore
             if (!Predict(kf, t))
                 return false;
 
-            nmath::Vector* z = m->Value;
-            nmath::Matrix* R = m->Covariance;
-            nmath::Matrix* H = m->ObservationModel;
-            nmath::Matrix* P = kf->covariance;
+            nmath::vector_t*   z = m->Value;
+            nmath::matrix_t* R = m->Covariance;
+            nmath::matrix_t* H = m->ObservationModel;
+            nmath::matrix_t* P = kf->covariance;
 
-            nmath::Vector* preFitResidual = nmath::NewVector(z->Len(), nullptr);
+            nmath::vector_t* preFitResidual = nmath::NewVector(z->Len(), nullptr);
             preFitResidual->MulVec(H, kf->state);
             preFitResidual->SubVec(z, preFitResidual);
 
-            nmath::Matrix* preFitResidualCov = nmath::NewMatrix(z->Len(), z->Len(), nullptr);
-            //     preFitResidualCov->Product(H, P, H->T());
-            //     preFitResidualCov->Add(preFitResidualCov, R);
+            nmath::matrix_t* preFitResidualCov = nmath::NewMatrix(z->Len(), z->Len(), nullptr);
+            nmath::matrix_t* transposedH       = nmath::Transpose(H);
+            preFitResidualCov->Product(H, P, transposedH);
+            preFitResidualCov->Add(preFitResidualCov, R);
 
-            nmath::Matrix* preFitResidualCovInv = nmath::NewMatrix(z->Len(), z->Len(), nullptr);
-            //     preFitResidualCovInv->Inverse(preFitResidualCov);
+            nmath::matrix_t* preFitResidualCovInv = nmath::NewMatrix(z->Len(), z->Len(), nullptr);
+            preFitResidualCovInv->Inverse(preFitResidualCov);
 
-            nmath::Matrix* gain = nmath::NewMatrix(kf->dims, z->Len(), nullptr);
-            //     gain->Product(P, H->T(), preFitResidualCovInv);
+            nmath::matrix_t* gain = nmath::NewMatrix(kf->dims, z->Len(), nullptr);
+            gain->Product(P, transposedH, preFitResidualCovInv);
 
-            nmath::Vector* newState = nmath::NewVector(kf->dims, nullptr);
+            nmath::vector_t* newState = nmath::NewVector(kf->dims, nullptr);
             newState->MulVec(gain, preFitResidual);
             newState->AddVec(kf->state, newState);
 
-            nmath::Matrix* newCovariance = nmath::NewMatrix(kf->dims, kf->dims, nullptr);
-            //     newCovariance->Mul(gain, H);
-            //     newCovariance->Sub(eye(kf->dims), newCovariance);
-            //     newCovariance->Mul(newCovariance, P);
+            nmath::matrix_t* newCovariance = nmath::NewMatrix(kf->dims, kf->dims, nullptr);
+            newCovariance->Mul(gain, H);
+            nmath::matrix_t* eye = Eye(kf, kf->dims);
+            newCovariance->Sub(eye, newCovariance);
+            newCovariance->Mul(newCovariance, P);
+            nmath::Free(eye);
 
-            //     kf->covariance = newCovariance;
-            //     kf->state = newState;
-            //     kf->t = t;
+            nmath::Free(kf->covariance);
+            nmath::Free(kf->state);
+
+            kf->covariance = newCovariance;
+            kf->state      = newState;
+            kf->t          = t;
 
             return true;
         }
 
-        nmath::Matrix* Eye(KalmanFilter* kf, s32 n)
+        nmath::matrix_t* Eye(filter_t* kf, s32 n)
         {
-            nmath::Matrix* result = nmath::NewMatrix(n, n, nullptr);
+            nmath::matrix_t* result = nmath::NewMatrix(n, n, nullptr);
             for (s32 i = 0; i < n; i++)
                 result->Set(i, i, 1.0);
             return result;
--- source/main/include/ckalman/c_kalman.h
+++ source/main/include/ckalman/c_kalman.h
@@ -7,87 +7,87 @@
 
 namespace ncore
 {
-    namespace nmath
-    {
-        struct Vector;
-        struct Matrix;
-    }  // namespace nmath
-
-    namespace nmodels
-    {
-        struct State
-        {
-            u64            Time;
-            nmath::Vector* State;
-            nmath::Matrix* Covariance;
-        };
-
-        struct Measurement
-        {
-            nmath::Matrix* Covariance;
-            nmath::Vector* Value;
-            nmath::Matrix* ObservationModel;
-        };
-
-        // LinearModel is used to initialize hidden states in the model and
-        // provide transition matrices to the filter.
-        // kalman/models provides commonly used models.
-        class LinearModel
-        {
-        public:
-            virtual void InitialState(State& state)                              = 0;
-            virtual void Transition(u64 dt, nmath::Matrix*& outMatrix)           = 0;
-            virtual void CovarianceTransition(u64 dt, nmath::Matrix*& outMatrix) = 0;
-        };
-    }  // namespace nmodels
-
     namespace nkalman
     {
-        // KalmanFilter is responsible for prediction and filtering
+        namespace nmath
+        {
+            struct vector_t;
+            struct matrix_t;
+        }  // namespace nmath
+
+        namespace nmodels
+        {
+            struct state_t
+            {
+                u64              Time;
+                nmath::vector_t* state_t;
+                nmath::matrix_t* Covariance;
+            };
+
+            struct measurement_t
+            {
+                nmath::matrix_t* Covariance;
+                nmath::vector_t* Value;
+                nmath::matrix_t* ObservationModel;
+            };
+
+            // model_t is used to initialize hidden states in the model and
+            // provide transition matrices to the filter.
+            // kalman/models provides commonly used models.
+            class model_t
+            {
+            public:
+                virtual void InitialState(state_t& state)                                = 0;
+                virtual void Transition(u64 dt, nmath::matrix_t*& outMatrix)           = 0;
+                virtual void CovarianceTransition(u64 dt, nmath::matrix_t*& outMatrix) = 0;
+            };
+        }  // namespace nmodels
+
+        // filter_t is responsible for prediction and filtering
         // of a given linear model. It is assumed that the process being modelled
         // is a time series, and that the time steps are non-uniform and specified
         // for each update and prediction operation.
-        struct KalmanFilter
+        struct filter_t
         {
-            nmodels::LinearModel* model;
-            s32                   dims;
-            u64                   t;
-            nmath::Vector*        state;
-            nmath::Matrix*        covariance;
+            nmodels::model_t* model;
+            s32                dims;
+            u64                t;
+            nmath::vector_t*   state;
+            nmath::matrix_t*   covariance;
         };
 
-        KalmanFilter* NewKalmanFilter(nmodels::LinearModel* model);
+        filter_t* NewFilter(nmodels::model_t* model);
 
-        // State returns the current hidden state of the KalmanFilter.
+        // state_t returns the current hidden state of the filter_t.
         // Example models provided with this package often provide functions
         // to extract meaningful information from the state vector, such as
         // .Velocity() for the provided constant velocity model.
-        nmath::Vector* State(KalmanFilter* kf);
+        nmath::vector_t* state_t(filter_t* kf);
 
         // Covariance returns the current covaraince of the model.
-        nmath::Matrix* Covariance(KalmanFilter* kf);
+        nmath::matrix_t* Covariance(filter_t* kf);
 
         // SetCovariance resets the covariance of the Kalman Filter to the given value.
-        void SetCovariance(KalmanFilter* kf, nmath::Matrix* covariance);
+        void SetCovariance(filter_t* kf, nmath::matrix_t* covariance);
 
         // SetState resets the state of the Kalman Filter to the given value.
-        void SetState(KalmanFilter* kf, nmath::Vector* state);
+        void SetState(filter_t* kf, nmath::vector_t* state);
 
         // Time returns the time for which the current hidden state is an estimate.
         // The time is monotone increasing.
-        u64 Time(KalmanFilter* kf);
+        u64 Time(filter_t* kf);
 
-        // Predict advances the KalmanFilter from the internal current time
+        // Predict advances the filter_t from the internal current time
         // to the given time using the built-in linear model.
         // The state of the filter is updated and the current time is updated.
         // Each time can be no earlier than the current time of the filter.
-        bool Predict(KalmanFilter* kf, u64 t);
+        bool Predict(filter_t* kf, u64 t);
 
         // Update is used to take a new measurement from a sensor and fuse it to the model.
         // The time field must be no earlier than the current time of the filter.
-        bool Update(KalmanFilter* kf, u64 t, nmodels::Measurement* m);
+        bool Update(filter_t* kf, u64 t, nmodels::measurement_t* m);
 
-        nmath::Matrix* Eye(KalmanFilter* kf, s32 n);
+        nmath::matrix_t* Eye(filter_t* kf, s32 n);
 
     }  // namespace nkalman
 }  // namespace ncore
